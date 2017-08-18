6b46bce189e127a8c22bfbcffcb99f7e726d295b
daxewing4 <30907333+daxewing4@users.noreply.github.com>
2017-08-18 09:36:15 -0500

    Weather Project
    Copied and pasted my code into this.

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,32 @@
 import java.util.Scanner;
 
-public class WeatherReport
+public class Weather_code
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+		System.out.println("Welcome! Please type in your temperature in the box below.");
 		
+		Scanner cin = new Scanner (System.in);
+		
+		//System.out.println(x);
+		
+		int m = cin.nextInt();
+		if (m<=32)
+		{
+			System.out.println("That's cold!");
+		}
+		else if (m>=90)
+		{
+			System.out.println("That's hot!");
+		}
+		double ans = 0;
+		
+		ans= ((m-32)*.5555555555556);
+		
+		System.out.println("The temperaure is " + (double)ans + " in Celsius!");
+		
+		cin.close();
 	}
 
 }
